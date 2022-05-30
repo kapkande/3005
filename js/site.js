9a80f249890a31d6e281fdf00603807edf0979b1
kapkande <101944501+kapkande@users.noreply.github.com>
2022-05-30 08:55:19 +0300

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,15 @@
+
+
+   function clik() {
+   // document.getElementsByClassName("burger_line").class.toggle("open");
+   // document.getElementsByClassName("burger_line").class.toggle("close");
+   document.getElementById("opens").classList.toggle("close");
+   document.getElementById("opens").classList.toggle("open");
+   document.getElementById("block-active").classList.toggle("block-active");
+   document.getElementById("block-active").classList.toggle("block-disabled");
+   document.getElementById("image-activ").classList.toggle("onFilter");
+   document.getElementById("image-activ").classList.toggle("offFilter");
+
+
+   
+   }
